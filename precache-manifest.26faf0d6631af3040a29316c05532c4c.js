af3aadc75f0a3f66d2796b6c027bc4c971e12abb
thisisbrix901 <bmadeira7@gmail.com>
2018-11-13 14:39:04 -0500

    Updates

---
+++ precache-manifest.26faf0d6631af3040a29316c05532c4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "04517435260a9af4bf19",
+    "url": "/clickGame/static/js/runtime~main.04517435.js"
+  },
+  {
+    "revision": "f41625c245bb910efaf4",
+    "url": "/clickGame/static/js/main.f41625c2.chunk.js"
+  },
+  {
+    "revision": "d715c167ec748181a577",
+    "url": "/clickGame/static/js/1.d715c167.chunk.js"
+  },
+  {
+    "revision": "f41625c245bb910efaf4",
+    "url": "/clickGame/static/css/main.aac9efd0.chunk.css"
+  },
+  {
+    "revision": "b1e9b796eed409343cef479448cf3242",
+    "url": "/clickGame/index.html"
+  }
+];
